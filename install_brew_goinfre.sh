2b8c15f80756ef80598a5aa2ec2c52e4fb6f4ef3
pvineet44 <pvineet44@gmail.com>
2020-12-29 12:10:17 +0100

    brew

--- install_brew_goinfre.sh
+++ install_brew_goinfre.sh
@@ -1,3 +1,3 @@
 rm -rf $HOME/.brew
-export path TMP_HOME=/goinfre
+export path TMP_HOME=~/goinfre
 rm -rf $TMP_HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $TMP_HOME/.brew && export PATH=$TMP_HOME/.brew/bin:$PATH && brew update && echo “export PATH=$TMP_HOME/.brew/bin:$PATH” >> ~/.zshrc
